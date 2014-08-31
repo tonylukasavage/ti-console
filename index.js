da302b01af190ad016830452502a3b886d34a797
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-08-31 17:02:59 -0400

    formatting

--- index.js
+++ index.js
@@ -1,86 +1,85 @@
 /*global window, global*/
-var util = require("util")
-var assert = require("assert")
-var now = require("date-now")
+var util = require("util");
+var assert = require("assert");
+var now = require("date-now");
 
-var slice = Array.prototype.slice
-var console
-var times = {}
+var console;
+var times = {};
 
 if (typeof global !== "undefined" && global.console) {
-    console = global.console
+	console = global.console;
 } else if (typeof window !== "undefined" && window.console) {
-    console = window.console
+	console = window.console;
 } else {
-    console = {}
+	console = {};
 }
 
 var functions = [
-    [log, "log"],
-    [info, "info"],
-    [warn, "warn"],
-    [error, "error"],
-    [time, "time"],
-    [timeEnd, "timeEnd"],
-    [trace, "trace"],
-    [dir, "dir"],
-    [consoleAssert, "assert"]
-]
+	[log, "log"],
+	[info, "info"],
+	[warn, "warn"],
+	[error, "error"],
+	[time, "time"],
+	[timeEnd, "timeEnd"],
+	[trace, "trace"],
+	[dir, "dir"],
+	[consoleAssert, "assert"]
+];
 
 for (var i = 0; i < functions.length; i++) {
-    var tuple = functions[i]
-    var f = tuple[0]
-    var name = tuple[1]
+	var tuple = functions[i];
+	var f = tuple[0];
+	var name = tuple[1];
 
-    if (!console[name]) {
-        console[name] = f
-    }
+	if (!console[name]) {
+		console[name] = f;
+	}
 }
 
-module.exports = console
+module.exports = console;
 
 function log() {}
 
 function info() {
-    console.log.apply(console, arguments)
+	console.log.apply(console, arguments);
 }
 
 function warn() {
-    console.log.apply(console, arguments)
+	console.log.apply(console, arguments);
 }
 
 function error() {
-    console.warn.apply(console, arguments)
+	console.warn.apply(console, arguments);
 }
 
 function time(label) {
-    times[label] = now()
+	times[label] = now();
 }
 
 function timeEnd(label) {
-    var time = times[label]
-    if (!time) {
-        throw new Error("No such label: " + label)
-    }
+	var time = times[label];
+	if (!time) {
+		throw new Error("No such label: " + label);
+	}
 
-    var duration = now() - time
-    console.log(label + ": " + duration + "ms")
+	var duration = now() - time;
+	console.log(label + ": " + duration + "ms");
 }
 
 function trace() {
-    var err = new Error()
-    err.name = "Trace"
-    err.message = util.format.apply(null, arguments)
-    console.error(err.stack)
+	var err = new Error();
+	err.name = "Trace";
+	err.message = util.format.apply(null, arguments);
+	console.error(err.stack);
 }
 
 function dir(object) {
-    console.log(util.inspect(object) + "\n")
+	console.log(util.inspect(object) + "\n");
 }
 
 function consoleAssert(expression) {
-    if (!expression) {
-        var arr = slice.call(arguments, 1)
-        assert.ok(false, util.format.apply(null, arr))
-    }
+	if (!expression) {
+		var arr = Array.prototype.slice.call(arguments, 1);
+		assert.ok(false, util.format.apply(null, arr));
+	}
 }
