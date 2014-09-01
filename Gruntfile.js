000189f2720bfa03bf2ad0bbaf5216e3380901d6
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-09-01 09:12:44 -0400

    properly finalize ti-console.js file

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,3 +1,5 @@
+var fs = require('fs');
+
 module.exports = function(grunt) {
 
   // Project configuration.
@@ -9,7 +11,7 @@ module.exports = function(grunt) {
       src: ['src/ti-console.js', 'test/**/*.js']
     },
     browserify: {
-      build: {
+      app: {
         files: {
           'ti-console.js': ['src/ti-console.js']
         }
@@ -39,19 +41,15 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-browserify');
   grunt.loadNpmTasks('grunt-titanium');
 
-  // run build
-  // grunt.registerTask('build', 'finalize ti-mocha.js release file', function() {
-  //   grunt.log.write('Reading build ti-mocha.js...');
-  //   var contents = fs.readFileSync(C.BUILD_FILE, 'utf8');
-  //   grunt.log.ok();
-
-  //   grunt.log.write('Copying "' + path.relative('.', C.BUILD_FILE) + '" to "' + path.relative('.', C.RELEASE_FILE) + '"...');
-  //   fs.writeFileSync(C.RELEASE_FILE, contents);
-  //   fs.chmodSync(C.RELEASE_FILE, fs.lstatSync(C.BUILD_FILE).mode);
-  //   grunt.log.ok();
-  // });
+  // finalize build
+  grunt.registerTask('finalize', 'finalize ti-console.js file', function() {
+    var code = fs.readFileSync('ti-console.js', 'utf8'),
+      match = code.match(/\[(\d+)\]\);?\s*$/);
+    fs.writeFileSync('ti-console.js', 'module.exports=' + code.replace(/;\s*$/,'') +
+      '(' + match[1] + ');');
+  });
 
   // lint and test node and titanium
-  grunt.registerTask('default', ['jshint', 'browserify', 'ti_run', 'clean']);
+  grunt.registerTask('default', ['jshint', 'browserify', 'finalize', 'ti_run', 'clean']);
 
 };
