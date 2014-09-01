e59f70e2f77dbc2f8340fbc3565539815c2edd14
Tony Lukasavage <anthony.lukasavage@gmail.com>
2014-09-01 09:22:28 -0400

    start adding console tests

--- test/app.js
+++ test/app.js
@@ -1,14 +1,26 @@
-var console = require('ti-console');
+var console = require('ti-console'),
+	should = require('should');
 require('ti-mocha');
 
 describe('ti-console', function() {
 
-	it('works', function() {
-		'foo'.should.equal('foo');
+	it('exports modules', function() {
+		should.exist(console);
+	});
+
+	it('exposes console functions', function() {
+		console.log.should.be.a.Function;
+		console.info.should.be.a.Function;
+		console.warn.should.be.a.Function;
+		console.dir.should.be.a.Function;
+		console.time.should.be.a.Function;
+		console.timeEnd.should.be.a.Function;
+		console.trace.should.be.a.Function;
+		console.assert.should.be.a.Function;
 	});
 
 });
 
 mocha.run(function() {
-	console.log('[TESTS COMPLETE]');
+	Ti.API.info('[TESTS COMPLETE]');
 });
